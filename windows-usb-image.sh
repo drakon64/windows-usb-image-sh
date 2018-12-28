5a118d94ba87ece47d5fd5b70791bac2b48b6f93
Adam Chance <adam.chance10@btinternet.com>
2018-12-28 23:39:38 +0000

    Show the help page if no arguments are given to the script

--- windows-usb-image.sh
+++ windows-usb-image.sh
@@ -142,14 +142,21 @@ windows()
 		rm "$CHECKSUM_FILE_WINDOWS"
 		cd "$CURRENT_PWD"
 		echo Unmounting the Windows partition
+		PASS=1
 	else
 		echo The Windows partition failed the checksum
 		rm "$CHECKSUM_FILE_WINDOWS"
 		cd "$CURRENT_PWD"
 		echo Unmounting the Windows partition
+		PASS=0
 	fi
 	umount "$WINDOWS"
 	rmdir "$WINDOWS"
+	if [ "$PASS" = 1 ] ; then
+		exit 0
+	else
+		exit 1
+	fi
 }
 
 dd_checksum()
@@ -205,3 +212,5 @@ while getopts "h:s:d:c:b:CD" arg ; do
 			;;
 	esac
 done
+
+usage
