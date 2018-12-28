d68d8a65b8d3072047db92b896e0dd9104ffa837
Adam Chance <adam.chance10@btinternet.com>
2018-12-28 20:38:08 +0000

    Improve the builtin help

--- windows-usb-image.sh
+++ windows-usb-image.sh
@@ -2,7 +2,22 @@
 
 usage()
 {
-	echo "Usage: $0 -s <iso file> -d <destination block device> -c <iso sha1 checksum> [-b <partiton block size>] [--dd <run in DD mode>] [-h <help>]"
+	echo 'windows-usb-image-sh
+
+	Required arguments:
+	-s    Source image file
+	-d    Destination block device (/dev/disk/by-id/)
+	-c    SHA1 checksum of source image file"
+	-C|-D Specify whether to use Copy Mode (-C) or DD Mode (-D)
+
+	Optional arguments:
+	-b    Partition block size
+
+	Copy Mode:
+	Copy Mode will create a 512KB FAT32 partition at the start of the block device, and an NTFS partition in the remaining space. The FAT32 partition contains the UEFI:NTFS bootloader, and the NTFS partition contains the source image file contents.
+
+	DD Mode:
+	DD Mode will use "dd" to clone the source image onto the destination block device. Copying will not be performed if the destination block devices checksum is the same as that of the source images.'
 	exit 0
 }
 
