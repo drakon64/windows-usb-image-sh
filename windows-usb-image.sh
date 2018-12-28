f5ffe1d86f505778d12429126106e3b4dfe77d1c
Adam Chance <adam.chance10@btinternet.com>
2018-12-28 20:41:44 +0000

    Update the `README.md` file

--- windows-usb-image.sh
+++ windows-usb-image.sh
@@ -4,20 +4,22 @@ usage()
 {
 	echo 'windows-usb-image-sh
 
-	Required arguments:
-	-s    Source image file
-	-d    Destination block device (/dev/disk/by-id/)
-	-c    SHA1 checksum of source image file"
-	-C|-D Specify whether to use Copy Mode (-C) or DD Mode (-D)
+Shell script for copying disk images to block devices
 
-	Optional arguments:
-	-b    Partition block size
+Required arguments:
+-s    Source image file
+-d    Destination block device (/dev/disk/by-id/)
+-c    SHA1 checksum of source image file"
+-C|-D Specify whether to use Copy Mode (-C) or DD Mode (-D)
 
-	Copy Mode:
-	Copy Mode will create a 512KB FAT32 partition at the start of the block device, and an NTFS partition in the remaining space. The FAT32 partition contains the UEFI:NTFS bootloader, and the NTFS partition contains the source image file contents.
+Optional arguments:
+-b    Partition block size
 
-	DD Mode:
-	DD Mode will use "dd" to clone the source image onto the destination block device. Copying will not be performed if the destination block devices checksum is the same as that of the source images.'
+Copy Mode:
+Copy Mode will create a 512KB FAT32 partition at the start of the block device, and an NTFS partition in the remaining space. The FAT32 partition contains the UEFI:NTFS bootloader, and the NTFS partition contains the source image file contents.
+
+DD Mode:
+DD Mode will use "dd" to clone the source image onto the destination block device. Copying will not be performed if the destination block devices checksum is the same as that of the source images.'
 	exit 0
 }
 
