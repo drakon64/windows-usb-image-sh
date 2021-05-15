75329ad043160815dd2704d82e1f19212d9b2455
Adam Chance <adam.chance10@live.co.uk>
2021-05-15 23:05:07 +0100

    Initial support for Copy Mode on macOS and BSD

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # windows-usb-image-sh
 
-Bash script for copying disk images to block devices
+Bash script for copying Windows ISO images (or any ISO) to block devices
 
 ## RPM build status
 
@@ -10,13 +10,13 @@ Bash script for copying disk images to block devices
 * `-s`    Source image file
 * `-d`    Destination block device
 * `-c`    SHA1 checksum of source image file
-* `-C|-D` Specify whether to use Copy Mode (`-C`) (Linux only) or DD Mode (`-D`)
+* `-C|-D` Specify whether to use Copy Mode (`-C`) or DD Mode (`-D`)
 
 ## Optional arguments
 * `-b`    Partition block size
 
-## Copy Mode (Linux only)
-Copy Mode will create a 512KB FAT32 partition at the start of the block device, and an NTFS partition in the remaining space. The FAT32 partition contains the UEFI:NTFS bootloader, and the NTFS partition contains the source image file contents.
+## Copy Mode
+Copy Mode will format the destination block device and create a FAT32 partition on it. This partition contains the source image file contents. The Windows `install.wim` file will be split into blocks of 1000MB to avoid FAT32 file size limitations.
 
 ## DD Mode
 DD Mode will use `dd` to clone the source image onto the destination block device. Copying will not be performed if the destination block devices checksum is the same as that of the source images.
--- windows-usb-image.sh
+++ windows-usb-image.sh
@@ -23,13 +23,17 @@ os()
 	UNAME="$(uname -s)"	
 
 	if [ "$UNAME" = "Linux" ] ; then
-		UNMOUNT=udisksctl unmount -b
+		UNMOUNT=udisksctl
+		UNMOUNT_ARGS=unmount -b
+
 		STAT=-c "%s"
 	elif [ "$UNAME" = "BSD" ] || [ "$UNAME" = "Darwin" ] ; then
 		if [ "$UNAME" = "Darwin" ] ; then
-			UNMOUNT=diskutil unmount
+			UNMOUNT=diskutil
+			UNMOUNT_ARGS=unmountDisk
 		else
-			UNMOUNT=udisksctl unmount -b
+			UNMOUNT=udisksctl
+			UNMOUNT_ARGS=unmount -b
 		fi
 		STAT=-f%z
 	else
@@ -41,7 +45,7 @@ os()
 unmount()
 {
 	echo Unmounting the USB
-	"$UNMOUNT" "$DISK"
+	"$UNMOUNT" "$UNMOUNT_ARGS" "$DISK"
 }
 
 iso_checksum()
@@ -59,17 +63,13 @@ iso_checksum()
 disk_mode()
 {
 	if [[ "$DISK" = /dev/disk/by-id/* ]] ; then
-		UEFI_PART=-part1
-		NTFS_PART=-part2
+		PART=-part1
 	elif [[ "$DISK" = /dev/sd* ]] || [[ "$DISK" = /dev/hd* ]] ; then
-		UEFI_PART=1
-		NTFS_PART=2
+		PART=1
 	elif [[ "$DISK" = /dev/nvme* ]] ; then
-		UEFI_PART=p1
-		NTFS_PART=p2
+		PART=p1
 	elif [[ "$DISK" = /dev/disk* ]] ; then
-		UEFI_PART=s1
-		NTFS_PART=s2
+		PART=s1
 	else
 		echo Unknown block device path
 		exit 1
@@ -79,119 +79,53 @@ disk_mode()
 cp_checksum()
 {
 	os
-
-	if ! [ "$UNAME" = "Linux" ] ; then
-		echo "Copy Mode is currently only supported under Linux"
-		exit 1
-	fi
-
 	unmount
 	iso_checksum
-	disk_mode
-
-	echo Partitioning the USB
-	(
-		echo g
-		echo n
-		echo
-		echo
-		echo +512K
-		echo t
-		echo 1
-		echo n
-		echo
-		echo
-		echo
-		echo t
-		echo 2
-		echo 1
-		echo w
-	) | fdisk "$DISK" || partprobe && sleep 3
-
-	echo Downloading UEFI:NTFS
-	UEFI_NTFS="$(mktemp)"
-	curl https://github.com/pbatard/rufus/raw/master/res/uefi/uefi-ntfs.img -o "$UEFI_NTFS"
-
-	if [ -z "$BLOCK_SIZE" ] ; then
-		echo Creating the Windows partition
-		mkfs.ntfs -Q "$DISK$NTFS_PART"
-	else
-		echo Creating the Windows partition
-		mkfs.ntfs -Q -s "$BLOCK_SIZE" "$DISK$NTFS_PART"
-	fi
 
 	echo Mounting the Windows ISO
-	LOOP=$(mktemp -d)
-	mount "$ISO" -o loop,ro "$LOOP"
+	if ! [ "$UNAME" = "Darwin" ] ; then
+		LOOP=$(mktemp -d)
+		mount "$ISO" -o loop,ro "$LOOP"
+	else
+		LOOP=$(hdiutil mount "$ISO" | awk '{ print $2 }')
+	fi
 
-	CURRENT_PWD=$(pwd)
+	echo Formatting the USB drive as FAT32
+	if [ "$UNAME" = "Darwin" ] ; then
+		diskutil eraseDisk FAT32 WIN10 MBR "$DISK"
+		PART_MOUNT=/Volumes/WIN10/
+	else
+		(
+			echo g
+			echo n
+			echo
+			echo
+			echo t
+			echo 2
+			echo 1
+			echo w
+		) | fdisk "$DISK" || partprobe && sleep 3
+		PART_MOUNT=$(mktemp -d)
+		mount -o loop "$DISK""$PART" "$PART_MOUNT"
+	fi
 
-	uefi &
-	windows &
-	wait
+	echo Copying the Windows 10 ISO files
+	rsync -qah --exclude=sources/install.wim "$LOOP"/* "$PART_MOUNT"
 
+	echo Splitting the Windows 10 install.wim file
+	wimsplit "$LOOP"/sources/install.wim /Volumes/WIN10/sources/install.swm 1000 --check
+
+	unmount || true
+	
 	echo Unmounting the Windows ISO
 	umount "$LOOP"
 
-	unmount || true
-
-	echo Cleaning up
-	rmdir "$LOOP"
-
+	if [ "$UNAME" = "Linux" ] ; then
+		echo Cleaning up
+		rmdir "$LOOP" "$PART_MOUNT" 
+	fi
+	
 	exit 0
-}
-
-uefi()
-{
-	os
-
-	echo Copying UEFI:NTFS
-	UEFI_NTFS_CHECKSUM=$(sha1sum "$UEFI_NTFS" | awk '{print $1}')
-	if [ -z "$BLOCK_SIZE" ] ; then
-		dd if="$UEFI_NTFS" of="$DISK$UEFI_PART" || echo Failed to copy UEFI:NTFS to the UEFI partition
-	else
-		dd if="$UEFI_NTFS" of="$DISK$UEFI_PART" bs="$BLOCK_SIZE" || echo Failed to copy UEFI:NTFS to the UEFI partition
-	fi
-	if [ "$(head -c "$(stat "$STAT" "$UEFI_NTFS")" "$DISK$UEFI_PART" | sha1sum | awk '{print $1}')" = "$UEFI_NTFS_CHECKSUM" ] ; then
-		echo The UEFI partition passed the checksum
-	else
-		echo The UEFI partition failed the checksum
-	fi
-	rm "$UEFI_NTFS"
-}
-
-windows()
-{
-	echo Generating checksums for the Windows partition files
-	CHECKSUM_FILE_WINDOWS=$(mktemp)
-	cd "$LOOP"
-	find . -type f -exec sha1sum {} \; >> "$CHECKSUM_FILE_WINDOWS"
-
-	echo Mounting the Windows partition
-	WINDOWS=$(mktemp -d)
-	mount "$DISK$NTFS_PART" "$WINDOWS"
-
-	echo Copying the Windows partition files
-	cp -r "$LOOP"/* "$WINDOWS"
-	cd "$WINDOWS"
-	echo Validating the Windows partition files
-	if sha1sum --status -c "$CHECKSUM_FILE_WINDOWS" ; then
-		echo The Windows partition passed the checksum
-	else
-		FAILED=true
-		echo The Windows partition failed the checksum
-	fi
-	rm "$CHECKSUM_FILE_WINDOWS"
-	cd "$CURRENT_PWD"
-	echo Unmounting the Windows partition
-	umount "$WINDOWS"
-	rmdir "$WINDOWS"
-
-	if ! [ -z "$FAILED" ] ; then
-		exit 0
-	else
-		exit 1
-	fi
 }
 
 dd_checksum()
